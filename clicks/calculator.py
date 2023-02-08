0871a163cdca6ad65c1712372d477c97f602bc05
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-02-08 11:20:10 +0900

    feat: using count option

--- clicks/calculator.py
+++ clicks/calculator.py
@@ -6,9 +6,26 @@ import click
 # multiple argument passing
 @click.command()
 @click.argument("xs", type=int, nargs=-1)
-def add(xs):
+# @click.option("-v", "--verbose", help="Show additional output.", is_flag=True)
+@click.option("-v", "--verbose", help="Show additional output.", count=True)
+def add(xs, verbose):
     """Adds numbers."""
-    click.echo(sum(xs))
+    if verbose > 1:  # vの数が入る(countの場合)
+        steps = []
+        results = 0
+        for x in xs:
+            steps.append(x)
+            results += x
+            click.echo(f"{' + '.join(str(s) for s in steps)} = {sum(steps)}")
+    elif verbose == 1:
+        click.echo(f"{' + '.join(str(x) for x in xs)} = {sum(xs)}")
+    else:
+        click.echo(sum(xs))
+
+    # if verbose: # booleanが入る(is_flagの場合)
+    #     click.echo(f"{' + '.join(str(s) for s in steps)} = {sum(steps)}")
+    # else:
+    #     click.echo(sum(xs))
 
 
 # @click.command()
@@ -21,12 +38,35 @@ def add(xs):
 # multiple argument passing
 @click.command()
 @click.argument("xs", type=int, nargs=-1)
-def subtract(xs):
+# @click.option("-v", "--verbose", help="Show additional output.", is_flag=True)
+@click.option("-v", "--verbose", help="Show additional output.", count=True)
+def subtract(xs, verbose):
     """Subtracts numbers."""
-    results = xs[0]
-    for x in xs[1:]:
-        results -= x
-    click.echo(results)
+    if verbose > 1:  # vの数が入る(countの場合)
+        z = xs[0]
+        steps = [z]
+        for x in xs[1:]:
+            steps.append(x)
+            z -= x
+            click.echo(f"{' - '.join(str(s) for s in steps)} = {z}")
+    else:
+        results = xs[0]
+        for x in xs[1:]:
+            results -= x
+
+        if verbose == 1:
+            click.echo(f"{' - '.join(str(x) for x in xs)} = {results}")
+        else:
+            click.echo(results)
+
+    # results = xs[0]
+    # for x in xs[1:]:
+    #     results -= x
+
+    # if verbose:# booleanが入る(is_flagの場合)
+    #     click.echo(f"{' - '.join(str(x) for x in xs)} = {results}")
+    # else:
+    #     click.echo(results)
 
 
 # @click.command()
