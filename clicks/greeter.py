6d28d9b8e0c9e1c777b40bc672e92bda73ee45bc
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-02-08 10:32:02 +0900

    feat: options

--- clicks/greeter.py
+++ clicks/greeter.py
@@ -4,8 +4,16 @@ import click
 
 
 @click.command()
-@click.argument("first_name")
-@click.argument("last_name")
-def greet(first_name, last_name):
+@click.argument("name")
+@click.option(
+    "--lang", help="Specify language English (en) or Spanish (es)", default="en"
+)
+def greet(name, lang):
     """Displays a greeting to the user."""
-    click.echo(f"Hello {first_name} {last_name}")
+    if lang == "es":
+        greetings = "Hola"
+    elif lang == "en":
+        greetings = "Hello"
+    else:
+        raise click.BadOptionUsage("lang", "Unsupported language.")
+    click.echo(f"{greetings} {name}")
