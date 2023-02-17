7464383d460fb56ec021492b015088328ceb29c0
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-02-17 10:38:11 +0900

    feat: solution1 (fn)

---
+++ fire/calc_cli.py
@@ -0,0 +1,29 @@
+import fire
+
+
+# Fxn Add
+def add(x, y):
+    """Add Numbers"""
+    return x + y
+
+
+# Fxn Subtract
+def subtract(x, y):
+    """Subtract Numbers"""
+    return x - y
+
+
+# Fxn Multiple
+def multiply(x, y):
+    """Multiply Numbers"""
+    return x * y
+
+
+# Fxn Divide
+def divide(x, y):
+    """Divide Numbers"""
+    return x / y
+
+
+if __name__ == "__main__":
+    fire.Fire()
