07bc03b432be6a39713466e80f9bbc0c6429db1d
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-02-17 10:37:33 +0900

    fmt: black is working

--- fire/basic_cli.py
+++ fire/basic_cli.py
@@ -7,22 +7,24 @@ def greet(name):
 
 def say_bye(name):
     print("Good Bye {}".format(name))
- 
+
+
 age = 24
 
 
 class Employee(object):
     """docstring for Employee"""
+
     def __init__(self, name):
         super(Employee, self).__init__()
         self.name = name
-    
+
     def __repr__(self):
         print("Employee(name={})".format(self.name))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # fire.Fire()  # Exposes Every Component
     # fire.Fire(greet)  # Exposes A Single Fxn
     # fire.Fire(Employee)  # Exposes A Single Class
-    fire.Fire({'greet': greet, 'saybye': say_bye})
+    fire.Fire({"greet": greet, "saybye": say_bye})
