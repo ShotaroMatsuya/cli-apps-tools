14c1ef50f76484807ce62de7e91d0a5d85a9cd12
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-02-17 10:44:26 +0900

    feat: solution2

--- fire/calc_cli.py
+++ fire/calc_cli.py
@@ -1,29 +1,33 @@
 import fire
 
 
-# Fxn Add
-def add(x, y):
-    """Add Numbers"""
-    return x + y
+class Calc(object):
+    def __init__(self, x, y):
+        """A Simple Calc CLI"""
+        super(Calc, self).__init__()
+        self.x = x
+        self.y = y
 
+    # Fxn Add
+    def add(self):
+        """Add Numbers"""
+        return self.x + self.y
 
-# Fxn Subtract
-def subtract(x, y):
-    """Subtract Numbers"""
-    return x - y
+    # Fxn Subtract
+    def subtract(self):
+        """Subtract Numbers"""
+        return self.x - self.y
 
+    # Fxn Multiple
+    def multiply(self):
+        """Multiply Numbers"""
+        return self.x * self.y
 
-# Fxn Multiple
-def multiply(x, y):
-    """Multiply Numbers"""
-    return x * y
-
-
-# Fxn Divide
-def divide(x, y):
-    """Divide Numbers"""
-    return x / y
+    # Fxn Divide
+    def divide(self):
+        """Divide Numbers"""
+        return self.x / self.y
 
 
 if __name__ == "__main__":
-    fire.Fire()
+    fire.Fire(Calc)
